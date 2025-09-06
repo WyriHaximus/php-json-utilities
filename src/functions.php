6ed1ca92a3aac97ef66213ca14ab76729b620de3
Cees-Jan Kiewiet <ceesjank@gmail.com>
2025-09-06 23:25:04 +0200

    Raise minimum PHP version to 8.4
    * Drop Safe
    * Update QA set up
    * GitHub config

--- src/functions.php
+++ src/functions.php
@@ -17,19 +17,22 @@ use function is_string;
 /**
  * @param array<string, mixed>                                   $data
  * @param array<string, string|array<string>>|array<int, string> $fields
+ *
+ * @phpstan-ignore ergebnis.noParameterWithNullableTypeDeclaration,ergebnis.noParameterWithNullDefaultValue
  */
-function validate_array(array $data, array $fields, ?string $exception = null): bool
+function validate_array(array $data, array $fields, string|null $exception = null): bool
 {
     $isInt = count(array_filter(array_keys($fields), 'is_int')) === count($fields);
 
     foreach ($fields as $field => $type) {
         if ($isInt) {
+            /** @phpstan-ignore shipmonk.variableTypeOverwritten */
             $field = $type;
             assert(is_string($field));
         }
 
-        /** @psalm-suppress PossiblyInvalidArrayOffset */
         if (
+            /** @phpstan-ignore ergebnis.noIsset */
             (! isset($data[$field]) && // This is faster,
                 // but it will also return false on fields which
                 // value is null, so calling array_key_exists when that happens.
@@ -49,10 +52,7 @@ function validate_array(array $data, array $fields, ?string $exception = null): 
                 return false;
             }
 
-            /**
-             * @psalm-suppress InvalidThrow
-             * @psalm-suppress InvalidStringClass
-             */
+            /** @phpstan-ignore throw.notThrowable */
             throw new $exception($data, $field);
         }
     }
